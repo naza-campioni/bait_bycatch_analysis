f742983d68ac1faffab14e360bcbf2ec60e3fe25
naza-campioni <56881474+naza-campioni@users.noreply.github.com>
2026-04-30 09:58:14 +0200

    Create calc_contingency.py

---
+++ functions/calc_contingency.py
@@ -0,0 +1,10 @@
+def calc_contingency(df: pd.DataFrame, feats: list, target: str):
+  '''
+  Calcualtes chi2 contingency test on dataframe.
+  '''
+
+  for feat in feats:
+    cont = pd.crosstab(df[feat], df[target])
+    chi2, p, dof, expected = chi2_contingency(cont)
+
+    print(f"{feat:20s}    chi2 = {chi2:.3f}, p = {p:.4f}")
